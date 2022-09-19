1433ed5ea0bd7ba58d99c36f26974bab3a4888d2
Marcus D. Leech <patchvonbraun@gmail.com>
2022-09-18 22:19:59 -0400

    Lordy.

--- process_carp_data.py
+++ process_carp_data.py
@@ -235,7 +235,7 @@ if (args.tpout != "" and args.tpout != None):
         normalarray = []
         for indx in range(0,len(lmstarrays[ndx])):
             if (lmstcounts[ndx][indx] > 0 and lmstarrays[ndx][indx] > 0.0):
-                normalarray.append(lmstarrays[ndx][indx]/lmstcounts[ndx][indx])
+                normalarray.append(lmstarrays[ndx][indx])
         normalarray = np.array(normalarray)
         mv = min(normalarray)
         lmstarrays[ndx] = np.divide(lmstarrays[ndx],mv)
