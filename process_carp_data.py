8e55ae7eda3d4eaabc2ecc6c7e55b8489a65cc3e
Marcus D. Leech <patchvonbraun@gmail.com>
2022-11-05 11:27:12 -0400

    Increased order of poly

--- process_carp_data.py
+++ process_carp_data.py
@@ -471,7 +471,7 @@ if (args.fftout != "" and args.fftout != ""):
         ctxarray = np.divide(ctxarray, np.min(ctxarray))
         
         ctxarray = scipy.signal.medfilt(ctxarray,kernel_size=177)
-        polyfit = np.polyfit(np.arange(0,len(ctxarray)),ctxarray,3)
+        polyfit = np.polyfit(np.arange(0,len(ctxarray)),ctxarray,5)
         p = np.poly1d(polyfit)
         newctx = []
         for x in np.arange(len(ctxarray)):
